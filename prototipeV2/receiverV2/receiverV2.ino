04d45f26c6fe68295e9b36d737546482c8678e8d
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2025-01-06 15:05:01 +0700

    iot: add wifi connection

--- prototipeV2/receiverV2/receiverV2.ino
+++ prototipeV2/receiverV2/receiverV2.ino
@@ -3,16 +3,45 @@
 #include <WiFi.h>
 #include "credentials.h"
 
-// Variabel untuk ThingerIoT
 ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
-// Waktu tunggu koneksi ulang (dalam milidetik)
 #define RECONNECT_DELAY 5000
 
 void setup()
 {
   Serial.begin(115200);
+
+  connectWiFi();
 }
 
 void loop()
 {
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    connectWiFi();
+  }
+}
+
+void connectWiFi()
+{
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, PASSWORD);
+
+  int attempt = 0;
+  while (WiFi.status() != WL_CONNECTED && attempt < 10)
+  {
+    delay(300);
+    Serial.print(".");
+    attempt++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("WiFi connected!");
+  }
+  else
+  {
+    Serial.println("Unable to connect to WiFi. Please check your network settings.");
+  }
 }
