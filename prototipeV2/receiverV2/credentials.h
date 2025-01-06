f9df73443780963bbdedd9678a6c6f990167922c
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2025-01-06 14:22:25 +0700

    iot: add credentials wifi and thingerio

---
+++ prototipeV2/receiverV2/credentials.h
@@ -0,0 +1,6 @@
+#define USERNAME "Justify24"
+#define DEVICE_ID "Serial_ThingerIoT_AgrinutriIoT_1"
+#define DEVICE_CREDENTIAL "T-RNK4S4IBZ9T4G0"
+
+#define SSID " ADVAN V1 PRO-863E0A"
+#define PASSWORD "03AF526F"
