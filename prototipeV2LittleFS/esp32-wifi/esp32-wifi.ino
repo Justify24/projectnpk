ac4adff936672537f7972fd6fb4a6543fbc0b005
Bancar Anggono Farros Santosa <bancarafarros@gmail.com>
2025-01-20 17:14:09 +0700

    iot: add second option to connect wifi

--- prototipeV2LittleFS/esp32-wifi/esp32-wifi.ino
+++ prototipeV2LittleFS/esp32-wifi/esp32-wifi.ino
@@ -6,18 +6,7 @@ ThingerESP32 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
 
 #include "credentials.h"
 
-void setup()
-{
-  Serial.begin(115200);
-
-  connectWiFi();
-}
-
-void loop()
-{
-}
-
-void connectWiFi()
+// void connectWiFi()
 {
   Serial.print("Connecting to ");
   Serial.println(SSID);
@@ -40,4 +29,53 @@ void connectWiFi()
   {
     Serial.println("Unable to connect to WiFi. Please check your network settings.");
   }
+}
+
+boolean connectWiFi()
+{
+  boolean connected = false;
+  Serial.print("Connecting to ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, PASSWORD);
+
+  int attempt = 0;
+  while (WiFi.status() != WL_CONNECTED && attempt < 10)
+  {
+    delay(300);
+    Serial.print(".");
+    attempt++;
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("WiFi connected!");
+    connected = true;
+  }
+  else
+  {
+    Serial.println("Unable to connect to WiFi. Please check your network settings.");
+  }
+
+  return connected;
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  boolean isConnected = connectWiFi();
+
+  if (isConnected)
+  {
+    Serial.println("Terhubung dengan WiFi, data akan dikirim ke thingerio");
+  }
+  else
+  {
+    Serial.println("Belum terhubung dengan WiFi, data akan dikirim ke LittleFS terlebih dahulu");
+  }
+}
+
+void loop()
+{
 }
